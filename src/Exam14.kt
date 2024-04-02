8cb9beae24c9d48dc0fd004be308b1febed81209
juyoung07 <yunju5090@gmail.com>
2024-04-02 18:49:54 +0900

    논리 연산자. 영어로 쓸 수 있음

--- src/Exam14.kt
+++ src/Exam14.kt
@@ -8,4 +8,18 @@ fun main() {
     println("result2: $result2")
     println("num1: $num1")
     println("num2: $num2")
+
+    val number1 = 12
+    val number2 = 25
+
+    println(number1 > 20 || number2 > 20)
+    println(number1 or number2)             // 비트 연산
+    // result = number1.or(number1)와 동일
+
+    println(number1 > 20 && number2 > 20)
+    println(number1 and number2)            // 비트 연산
+    // result = number1.and(number1)와 동일
+
+    println(number1 xor number2)    // 비트 연산
+    // result = number1.xor(number1)와 동일
 }
