a9898cb81a6e5258a2efc904d823a98323304232
juyoung07 <yunju5090@gmail.com>
2024-04-02 22:44:27 +0900

    점수를 입력받아 90점 이상이면 A, 80점 이상이면 B, 70점 이상이면 C, 60점 이상이면 D, 나머지는 F를 출력하는 프로그램

---
+++ src/IfTest02.kt
@@ -0,0 +1,22 @@
+fun main() {
+    print("점수를 입력하세요 >> ")
+    var score: Int
+    var grade: String
+    try {
+        score = readln().toInt()
+        grade = if (score >= 90)
+                "A"
+            else if (score in 80..90)
+                "B"
+            else if (score in 70..80)
+                "C"
+            else if (score in 60..70)
+                "D"
+            else
+                "F"
+
+        println("$score 점은 $grade 입니다.")
+    } catch (e: Exception) {
+        println("null")
+    }
+}
