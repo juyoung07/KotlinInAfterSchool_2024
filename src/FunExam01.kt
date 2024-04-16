e58efc5551d2caf0c8d21e1c9e7138ec142467bb
juyoung07 <yunju5090@gmail.com>
2024-04-16 18:59:11 +0900

    함수, 간략 함수

---
+++ src/FunExam01.kt
@@ -0,0 +1,12 @@
+fun sumBasic(a:Int, b:Int) : Int {
+    return a + b
+}
+fun sum(a:Int, b:Int) = a + b   // 단일 표기식
+fun main() {
+    print("정수1 입력 >> ");    var x = readln().toInt()
+    print("정수2 입력 >> ");    var y = readln().toInt()
+    println("$x + $y = ${sum(x, y)}")
+
+    var result = sum(1, 2)
+    println("1 + 2 = $result")
+}
