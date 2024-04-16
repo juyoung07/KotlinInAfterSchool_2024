3642f5ac68ecdc21eff3410a47ec2e37200079b2
juyoung07 <yunju5090@gmail.com>
2024-04-16 19:51:58 +0900

    문자열 이름들을 가변적인 개수로 받아서 유니코드 글자수가 3개인 문자열을 만날 때, 이를 출력하고 마치는 함수

---
+++ src/FunTest02.kt
@@ -0,0 +1,13 @@
+fun findFirstName(vararg Names: String) {
+    for (name in Names) {
+        if (name.length == 3) {
+            println("Found $name")
+            return
+        }
+    }
+}
+
+fun main() {
+    var s = arrayOf<String>("abcdefg", "aaa", "dd")
+    findFirstName(*s)
+}
