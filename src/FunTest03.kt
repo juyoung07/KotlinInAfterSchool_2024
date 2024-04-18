061434bb6c7f685cc92885ce7f5f3f7ae0283b85
juyoung07 <yunju5090@gmail.com>
2024-04-18 18:51:22 +0900

    두 수와 연산자를 입력 받아 인자로 넘기면, 계산결과를 반환하는 함수를 작성하고 반환값을 출력하는 계산기

---
+++ src/FunTest03.kt
@@ -0,0 +1,25 @@
+fun sum(a: Int, b: Int, op: String): Int {
+    var result = 0
+    when (op) {
+        "+" -> result = a + b
+        "-" -> result = a - b
+        "*" -> result = a * b
+        "/" -> {
+            if (b != 0)
+                result = a / b
+            else println("0으로 나눌 수 없습니다.")
+        }
+        else -> println("+ - * / 연산자만 입력하세요")
+    }
+    return result
+}
+
+fun main() {
+    print("숫자 1 >> ")
+    var a = readln().toInt()
+    print("숫자 2 >> ")
+    var b = readln().toInt()
+    print("연산자 >> ")
+    var op = readln()
+    println("$a + $b = ${sum(a, b, op)}")
+}
