c3865e04205302b3d997d05973ab944523443967
juyoung07 <yunju5090@gmail.com>
2024-04-23 18:44:10 +0900

    고차함수 (HOF: Higher Order Functions)

---
+++ src/HOFExam01.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val res1 = sum(3,2)
+    val res2 = mul(sum(3, 3), 3)
+    println("res1 = $res1, res2 = $res2")
+    println("func: ${func()}")
+}
+
+fun sum(a: Int, b: Int) = a + b
+fun mul(a: Int, b: Int) = a * b
+fun func(): Int {
+    return sum(2, 2)
+}
