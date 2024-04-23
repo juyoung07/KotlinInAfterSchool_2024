776064c6631e64f2c15f2784c30c31a05a0953ee
juyoung07 <yunju5090@gmail.com>
2024-04-23 19:11:55 +0900

    함수포인터

--- src/HOFExam02.kt
+++ src/HOFExam02.kt
@@ -1,3 +1,7 @@
+fun sumC(a: Int, b: Int): Int = a + b       // {a, b -> a + b} 이렇게 넘겨짐
+
+fun calc(a: Int, b: Int, operation: (Int, Int) -> Int) = operation(a, b)
+
 fun a(str: String): String {
     return str
 }
@@ -6,5 +10,6 @@ fun b(funs: (String) -> String) {
     println(funs("awds"))
 }
 fun main() {
-    b(::a)
+    b(::a)          // 고차함수 호출: 사용할 함수를 매개변수로 넘겨줌 -> ::(함수포인터) 활용
+    println(calc(2, 1, ::sumC))
 }
