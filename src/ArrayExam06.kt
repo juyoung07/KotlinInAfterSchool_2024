967c4d11ee2681f97f101691c9d61cded39c30e0
juyoung07 <yunju5090@gmail.com>
2024-05-07 19:50:28 +0900

    배열 함수

---
+++ src/ArrayExam06.kt
@@ -0,0 +1,9 @@
+import java.util.*
+
+fun main() {
+    val items = arrayOf<String>("Dog", "Cat", "Lion", "Kangaroo", "Po")
+    items.sortBy { item -> item.length }
+    println(Arrays.toString(items))
+    items.sortByDescending { item -> item.length }
+    println(Arrays.toString(items))
+}
--- src/Chapter04/ArrayExam05.kt
+++ src/Chapter04/ArrayExam05.kt
@@ -1,3 +1,5 @@
+package Chapter04
+
 import java.util.*
 
 fun main() {
