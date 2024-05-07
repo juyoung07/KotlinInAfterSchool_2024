10fe628839d79122fa2fc66f25f836a36b44a853
juyoung07 <yunju5090@gmail.com>
2024-05-07 19:19:27 +0900

    반복문을 사용한 배열 순환

--- src/ArrayExam03.kt
+++ src/ArrayExam03.kt
@@ -1,9 +1,23 @@
 fun main() {
-    var names = arrayOfNulls<String>(4)
-    names[0] = "Kim"
-    names.set(1, "Kang")
+//    var names = arrayOfNulls<String>(4)
+//    names[0] = "Kim"
+//    names.set(1, "Kang")
+//
+//    var n0 = names[0]
+//    var n1 = names.get(1)
+//    println("$n0, $n1")
 
-    var n0 = names[0]
-    var n1 = names.get(1)
-    println("$n0, $n1")
+    var names = arrayOf<String>("Java", "Python", "HTML", "Kotlin")
+
+    for (i in 0..names.size-1)
+        println("names[$i] = ${names[i]}")
+
+    for (i in names)
+        println(i)
+
+    for (i in names.indices)                // 인덱스만 나옴
+        println(i)
+
+    for ((i, x) in names.withIndex())       // 인덱스와 값
+        print("names[$i] = $x \t")
 }
