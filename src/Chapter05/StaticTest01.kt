6a27a0c13ad881b6ae3aa62c26858ec6c445ce10
juyoung07 <yunju5090@gmail.com>
2024-05-28 19:56:08 +0900

    companion object (static) 실습

--- src/Chapter05/StaticTest01.kt
+++ src/Chapter05/StaticTest01.kt
@@ -1,4 +1,6 @@
-class Card(var name: String, var kind: String = "Spade", var num: Int = 1) {
+package Chapter05
+
+class Card(var kind: String = "Spade", var num: Int = 1) {
     companion object {
         var width: Int = 100
         var height: Int = 250
@@ -6,17 +8,18 @@ class Card(var name: String, var kind: String = "Spade", var num: Int = 1) {
         fun showSize() = println("카드의 폭은: $width, 높이는 $height")
     }
 
-    fun show() {
-        println("$name >> 무늬 : $kind 값 : $num")
+    fun show(name: String) {
+        println("${name} >> 무늬 : $kind 값 : $num")
     }
 }
 
 fun main() {
     Card.showSize()
-    var c1 = Card("c1","Heart", 7)
-    c1.show()
-    var c2 = Card("c2S")
-    c2.show()
+    var c1 = Card("Heart", 7)
+    var c2 = Card()
+    c1.show("c1")
+    c2.show("c2")
+
     Card.width = 50
     Card.height = 80
     Card.showSize()
