f3c30086057a7d6df6333d67b78106051a8ec4de
juyoung07 <yunju5090@gmail.com>
2024-05-30 18:42:47 +0900

    Data class..

---
+++ src/DataClassExam01.kt
@@ -0,0 +1,9 @@
+data class UserData(val name: String, var age: Int)
+
+fun main() {
+    var userData = UserData("Michael", 21)
+    println(userData.toString())
+
+    var newUser = userData.copy()
+    println(newUser.toString())
+}
