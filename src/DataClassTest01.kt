89b755503b74623356ee3fbc744594fb678b2180
juyoung07 <yunju5090@gmail.com>
2024-05-30 19:03:02 +0900

    data class Training

---
+++ src/DataClassTest01.kt
@@ -0,0 +1,30 @@
+data class Employee(var name: String, var age: Int, var salary: Int = 1000)
+fun avgAge(arr: ArrayList<Employee>) : Double {
+    var sum = 0
+    for (i in arr) {
+        sum += i.age
+    }
+    return (sum.toDouble() / arr.size)
+}
+fun avgSalary(arr: ArrayList<Employee>) : Double {
+    var sum = 0
+    for (i in arr) {
+        sum += i.salary
+    }
+    return (sum.toDouble() / arr.size)
+}
+
+fun main() {
+    var arr: ArrayList<Employee> = ArrayList()
+    arr.add(Employee("Kim", 35, 5000))
+    arr.add(Employee("Hone", 25))
+    arr.add(Employee("Lee", 55, 10000))
+    arr.add(Employee("Cha", 21, 1000))
+
+    for (i in arr) {
+        println("name = ${i.name}, age = ${i.age}, salary = ${i.salary}")
+    }
+
+    println("평균나이 : ${avgAge(arr)}")
+    println("평균나이 : ${avgSalary(arr)}")
+}
